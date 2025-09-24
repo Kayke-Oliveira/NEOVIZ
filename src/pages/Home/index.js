626df00c93d96071e7372c2d77c1519c028c3774
Kayke-Oliveira <163776794+Kayke-Oliveira@users.noreply.github.com>
2025-09-24 20:32:07 -0300

    Home Page feita e estilizada, porém ainda são necessárias algumas melhorias nas cores e transições.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,9 +5,38 @@ import './home.css'
 
 function Home() {
     return (
-        <div>
-            
-        </div>
+        <section id='First'>
+            <div className='container-center'>
+                <h1> Inovação Digital </h1>
+                <h2> <span> Para Pe</span>quenos Negócios  </h2> <br />
+
+                <p> Transformamos ideias em experiências digitais extraordinárias. <strong> Design elegante, tecnologia avançada </strong> e <span> resultados que impactam seu negócio. </span> </p> <br />
+
+                <div className='button-group'>
+                    <button className='button-esquerda'> Transformar Meu Negócio →  </button>
+                    <button className='button-direita'> Ver Nossos Serviços </button>
+                </div>
+                <br />
+
+                <div className='resultados'>
+
+                    <p> CONFIANÇA E RESULTADOS </p> <br />
+                    <ul>
+                        <li> +50 Projetos Entregues </li>
+                        <li> 100% Satisfação </li>
+                        <li> Design Award Winer </li>
+                    </ul>
+                </div>
+
+                <div class="mouse-icon">
+                    <span class="scroll"></span>
+                </div>
+
+
+            </div>
+        </section>
+        
+
     )
 }
 
